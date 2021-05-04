e590a21dcd7e53410f40d4e78a34c094050c9512
amaiga <abdoulrazackmaiga9@gmail.com>
2021-05-04 16:20:49 +0000

    |_add model

--- disquaire/disquaire_project/store/models.py
+++ disquaire/disquaire_project/store/models.py
@@ -3,17 +3,57 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.db import migrations, models
+
 # Create your models here.
-ARTISTS = {
-  'francis-cabrel': {'name': 'Francis Cabrel'},
-  'lej': {'name': 'Elijay'},
-  'rosana': {'name': 'Rosana'},
-  'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
-}
+#ARTISTS = {
+ # 'francis-cabrel': {'name': 'Francis Cabrel'},
+  #'lej': {'name': 'Elijay'},
+  #'rosana': {'name': 'Rosana'},
+  #'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
+#}
+
+#ALBUMS = [
+ # {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
+  #{'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
+  #{'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
+#]
+class Artist(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=200)
 
 
-ALBUMS = [
-  {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-  {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-  {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
-]
+class Album(models.Model):
+    reference = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    picture = models.URLField()
+    artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+
+
+class Booking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
+    album = models.OneToOneField(Album,on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Artist',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+    ]
--- disquaire/disquaire_project/store/urls.py
+++ disquaire/disquaire_project/store/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from . import views # import views so we can use them in urls.
 urlpatterns = [
-    url(r'^$', views.listing),
-    url(r'^(?P<album_id>[0-9])/$',views.getAlbum)
+    #url(r'^$', views.listing),
+    #url(r'^(?P<album_id>[0-9])/$',views.getAlbum),
+    #url(r'^search/$', views.search),
 ]
